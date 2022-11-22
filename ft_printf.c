1f594c2881eba6a740395a273fc7462dcba9d474
kpuwar <kpuwar@student.42heilbronn.de>
2022-11-22 16:30:25 +0100

    uxXp pending

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/20 14:12:56 by kpuwar            #+#    #+#             */
-/*   Updated: 2022/11/22 13:51:06 by kpuwar           ###   ########.fr       */
+/*   Updated: 2022/11/22 15:41:55 by kpuwar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,29 @@ int	ft_printf(const char *args, ...)
 {
 	va_list	valist;
 	int		num;
+	int		len;
 
+	len = 0;
 	num = count_args(args);
 	va_start(valist, num);
 	while (*args)
 	{
 		if (*args == '%')
-			print_var(*(++args), valist);
+			len += print_var(*(++args), valist);
 		else
+		{
 			write(1, args, 1);
+			len++;
+		}
 		args++;
 	}
 	va_end(valist);
-	return (0);
+	return (len);
 }
 
 int main(void)
 {
-	ft_printf("%s %d %x %d\n", "hello\nworld\nhow are you\t??", 50, -100, 90);
+	printf("%d\n", ft_printf("%s %d %s %u%c", "hello world ", 50, "\nhow are you?? ", -100, '\n'));
+	printf("%d", printf("%s %d %s %u%c", "hello world ", 50, "\nhow are you?? ", -100, '\n'));
 	return 0;
 }
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,7 @@
 /*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 08:56:08 by kpuwar            #+#    #+#             */
-/*   Updated: 2022/11/22 13:48:13 by kpuwar           ###   ########.fr       */
+/*   Updated: 2022/11/22 16:29:26 by kpuwar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,6 @@ int	count_args(const char *str)
 		str++;
 	}
 	return (count);
-}
-
-int	print_var(char c, va_list valist)
-{
-	if (c == 'c')
-		return (ft_putchar_fd(va_arg(valist, char), 1));
-	else if (c == 's')
-		return (ft_putstr_fd(va_arg(valist, char *), 1));
-	else if (c == 'p')	//pending
-		return (ft_putstr_fd(va_arg(valist, char *), 1));
-	else if (c == 'd' || c == 'i') //length pending
-		return (ft_putnbr_fd(va_arg(valist, int), 1));
-	else if (c == 'u') //full pending
-		return (ft_putnbr_fd((unsigned int) va_arg(valist, int), 1));
-	else if (c == 'x')	//num to hex pending
-		return (ft_putnbr_fd(va_arg(valist, int), 1));
-	else if (c == 'X')	//cap hex pending
-		return (ft_putnbr_fd(va_arg(valist, int), 1));
-	else if (c == '%')
-		return (ft_putchar_fd('%', 1));
 }
 
 size_t	ft_strlen(const char *s)
@@ -75,23 +55,19 @@ int	ft_putchar_fd(char c, int fd)
 
 int	ft_putstr_fd(char *s, int fd)
 {
-	int	len = ft_strlen(s);
-	write(fd, s, sizeof(char) * len);
-	return (len);
+	return (write(fd, s, sizeof(char) * ft_strlen(s)));
 }
 
 int	ft_putnbr_fd(int n, int fd)
 {
 	char	arr[10];
 	long	ln;
+	int		len;
 
 	ln = n;
 	n = 0;
 	if (ln == 0)
-	{
-		write(fd, "0", 1);
-		return (1);
-	}
+		return (write(fd, "0", 1));
 	ft_memset(arr, '0', 10);
 	if (ln < 0)
 	{
@@ -103,7 +79,35 @@ int	ft_putnbr_fd(int n, int fd)
 		arr[n++] = (char)((ln % 10) + '0');
 		ln /= 10;
 	}
+	len = n;
 	while (n--)
 		write(fd, &arr[n], 1);
-	return (0);
+	return (len);
+}
+
+char	*tohex(int n)
+{
+	//pending
+}
+
+//toupper fn
+
+int	print_var(char c, va_list valist)
+{
+	if (c == 'c')
+		return (ft_putchar_fd(va_arg(valist, int), 1));
+	else if (c == 's')
+		return (ft_putstr_fd(va_arg(valist, char *), 1));
+	else if (c == 'p')	//pending
+		return (ft_putstr_fd(va_arg(valist, char *), 1));
+	else if (c == 'd' || c == 'i')
+		return (ft_putnbr_fd(va_arg(valist, int), 1));
+	else if (c == 'u') //conversion
+		return (ft_putnbr_fd(va_arg(valist, unsigned int), 1));
+	else if (c == 'x')	//num to hex pending
+		return (ft_putnbr_fd(va_arg(valist, int), 1));
+	else if (c == 'X')	//cap hex pending
+		return (ft_putnbr_fd(va_arg(valist, int), 1));
+	else
+		return (ft_putchar_fd('%', 1));
 }
